0a148f6dc3151d9770e84a6b5dc7350db5ef74d4
Jonathan Miranda <miranda.jth@gmail.com>
2020-04-30 21:19:03 -0600

    creating new controller for Collection Controller and Collection Cell adding imageView and defining constraints

---
+++ MemeMe1.0/MemeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe1.0
+//
+//  Created by Jonathan Miranda on 4/30/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    
+}
