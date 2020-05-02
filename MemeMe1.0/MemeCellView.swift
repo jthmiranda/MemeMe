2388b00602f5ffe6735d290f2c07990f869725c3
Jonathan Miranda <miranda.jth@gmail.com>
2020-05-01 20:54:51 -0600

    replacing programmatic modal ImagePicker instead setting a segue to show modal-fullscreen to reach viewWillAppear to reloadData defining a custom table cell controller y modifying storyboard to show properly

---
+++ MemeMe1.0/MemeCellView.swift
@@ -0,0 +1,17 @@
+//
+//  MemeCellView.swift
+//  MemeMe1.0
+//
+//  Created by Jonathan Miranda on 5/1/20.
+//  Copyright © 2020 Jonathan Miranda. All rights reserved.
+//
+
+import UIKit
+
+class MemeCellView: UITableViewCell {
+
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topText: UILabel!
+    @IBOutlet weak var bottonText: UILabel!
+    
+}
--- MemeMe1.0/MemeTableViewController.swift
+++ MemeMe1.0/MemeTableViewController.swift
@@ -11,29 +11,43 @@ import UIKit
 class MemeTableViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     
+    @IBOutlet weak var memeTableView: UITableView!
     
-    let memes = ((UIApplication.shared.delegate) as! AppDelegate).memes
+    var memes: [Meme] {
+        let appDelegate = UIApplication.shared.delegate
+        let object = appDelegate as! AppDelegate
+        return object.memes
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .done, target: self, action: #selector(MemeTableViewController.showMemeEditor))
+//        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "plus"), style: .done, target: self, action: #selector(MemeTableViewController.showMemeEditor))
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.memeTableView.reloadData()
+    }
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.memes.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeCell")!
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MemeCell") as! MemeCellView
         let meme = self.memes[(indexPath as NSIndexPath).row]
-        cell.textLabel?.text = meme.topText
-        cell.detailTextLabel?.text = meme.bottomText
+        
+        cell.memeImageView.image = meme.memedImage
+        cell.topText.text = meme.topText
+        cell.bottonText.text = meme.bottomText
+        
         return cell
     }
  
-    @objc func showMemeEditor() {
-        let memeEditorControler = self.storyboard?.instantiateViewController(identifier: "MemeEditor") as! MemeEditorViewController
-        present(memeEditorControler, animated: true)
-    }
+//    @objc func showMemeEditor() {
+//        let memeEditorControler = self.storyboard?.instantiateViewController(identifier: "MemeEditor") as! MemeEditorViewController
+//        present(memeEditorControler, animated: true)
+//    }
 }
