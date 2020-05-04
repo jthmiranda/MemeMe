9f4bde7092881ef1cf2da3aca9ed012e3f7d0b20
Jonathan Miranda <miranda.jth@gmail.com>
2020-05-03 21:52:12 -0600

    showing images on tableview and collection view works now! so it needs to adjust

--- MemeMe1.0/MemeCollectionCollectionViewController.swift
+++ MemeMe1.0/MemeCollectionCollectionViewController.swift
@@ -19,10 +19,18 @@ class MemeCollectionCollectionViewController: UICollectionViewController {
     }
 
     @IBOutlet var memeCollectionView: UICollectionView!
+    @IBOutlet weak var flowLayout: UICollectionViewFlowLayout!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let space: CGFloat = 3.0
+        let dimension = (view.frame.size.width - (2  * space)) / 3.0
+        
+        flowLayout.minimumInteritemSpacing = space
+        flowLayout.minimumLineSpacing = space
+        flowLayout.itemSize = CGSize(width: dimension, height: dimension)
     }
     
     override func viewWillAppear(_ animated: Bool) {
